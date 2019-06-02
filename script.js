f36b6610933a9252e0058ddc896a96875a438a4e
Araksiya <araksiya.og@gmail.com>
2019-06-02 15:00:34 +0300

    My first commit

---
+++ script.js
@@ -0,0 +1,46 @@
+console.log('animals');
+console.group('cats');
+console.log('cats color');
+console.groupEnd("");
+console.group('dogs');
+console.log('dogs color');
+console.groupEnd("");
+
+
+var name = 'animals'
+console.log(name);
+console.group('cats');
+console.log('cats color');
+console.groupEnd("");
+
+//////////////////////////////////////////////
+console.error('error');
+
+var text = 'Error';
+console.error(text);
+////////////////////////////////////////////
+console.info('Attention stop');
+
+var name = 'stop';
+var text = 'Attention ' +name;
+console.info(text);
+
+//////////////////////////////////////////////
+console.log('Hello Asya');
+
+var name = 'Asya';
+var text = 'Hello ' + name;
+console.log(text);
+
+///////////////////////////////////////////////
+console.warn('Warning');
+
+var text = 'Warning';
+console.warn(text);
+
+
+
+
+
+
+
